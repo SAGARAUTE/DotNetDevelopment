7188659d3fcb86f5936034cf582cdb2e58d3e660
SAGARAUTE <sagaraute1717@gmail.com>
2021-10-12 23:04:15 +0530

    Program.cs added

---
+++ DeveloperInterviewExercise/FileData/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+         FileDetails fileDetails = new FileDetails();
+         List<string> versionInput = new List<string> { "-v","--v", "/v", "--version" };
+         List<string> sizeInput = new List<string> { "-s", "--s", "/s", "--size" };
+         
+         #region Task1 
+         
+         if (args != null)
+         {
+            if(args[0].Equals("-v"))
+            {
+               Console.WriteLine("Task1 => FileVersion: " + fileDetails.Version(args[1]) + "\n \n");
+            }
+         }
+         #endregion
+
+         #region Task2
+         if (args != null)
+         {
+            if (versionInput.Contains(args[0]))
+            {
+               Console.WriteLine("Task2 => FileVersion: " + fileDetails.Version(args[1]));
+            }
+            else if(sizeInput.Contains(args[0]))
+            {
+               Console.WriteLine("Task2 => FileSize: " + fileDetails.Size(args[1]));
+            }
+         }
+         #endregion
+         Console.ReadKey();
+        }
+    }
+}
